5e261bd4aa751c3729adc3008c91fd252088509a
Mihail <misha.romanchuk.2015@mail.ru>
2022-10-28 00:21:30 +0300

    fixed hw1

--- hw1.py
+++ hw1.py
@@ -1,10 +1,22 @@
-a, b, c = map(int, input().split())
+a, b, c = input().split()
 
-d = b**2 - 4*a*c
+if not(a.isdigit()) or not(b.isdigit()) or not(c.isdigit()):
 
-if d < 0:
-    print("Корней нет")
-elif d == 0:
-    print(f"x = {-b/(2 * a)}")
+    print("Введенеы некорректные данные")
+
 else:
-    print(f"x1 = {(-b + d**0.5)/(2 * a)}, x2 = {(-b - d**0.5)/(2 * a)}")
+
+    a, b, c = map(int, (a, b, c))
+    if a == 0:
+
+        print(f"x = {(-c/b)}")
+
+    
+    else:
+        d = b**2 - 4*a*c
+        if d < 0:
+            print("Корней нет")
+        elif d == 0:
+            print(f"x = {-b/(2 * a)}")
+        else:
+            print(f"x1 = {(-b + d**0.5)/(2 * a)}, x2 = {(-b - d**0.5)/(2 * a)}")
