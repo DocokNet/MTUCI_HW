91bcf46ceb5b5264e038f78a7d3dbbb2693a2e94
Mihail <misha.romanchuk.2015@mail.ru>
2022-11-06 18:29:02 +0300

    fixed labs 4 and 5

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import requests
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import psycopg2
 
 
@@ -13,23 +13,39 @@ conn = psycopg2.connect(database="service_db",
 
 cursor = conn.cursor()
 
-
-@app.route('/login/', methods=['POST'])
+@app.route('/login/', methods=['POST', 'GET'])
 def login():
-    username = request.form.get('username')
-    password = request.form.get('password')
-    if username == '' or password == '':
-        return render_template('empty_error.html', empty_error='Some authorisation fields are empty!')
-    else:   
-        cursor.execute("SELECT * FROM service.users WHERE login=%s AND password=%s", (str(username), str(password)))
-        records = list(cursor.fetchall())
-        if records == []:
-            return render_template('missing_user.html', missing_user_error='There is no such user registrated!')
-        else:
-            return render_template('account.html', full_name=f'{records[0][1]}', login_pass=f'Login = {records[0][2]}, password = {records[0][3]}')
-
-
-@app.route('/login/', methods=['GET'])
-def index():
+    if request.method == 'POST':
+        if request.form.get("login"):
+            username = request.form.get('username')
+            password = request.form.get('password')
+            if username == '' or password == '':
+                return render_template('error_page.html', error='Some authorisation fields are empty!')
+            else:   
+                cursor.execute("SELECT * FROM service.users WHERE login=%s AND password=%s", (str(username), str(password)))
+                records = list(cursor.fetchall())
+                if records == []:
+                    return render_template('error_page.html', error='There is no such user registrated!')
+                else:
+                    return render_template('account.html', full_name=f'{records[0][1]}', login_pass=f'Login = {records[0][2]}, password = {records[0][3]}')
+        elif request.form.get("registration"):
+            return redirect("/registration/")
     return render_template('login.html')
+
+
+@app.route('/registration/', methods=['POST', 'GET'])
+def registration():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        login = request.form.get('login')
+        password = request.form.get('password')
+        cursor.execute("SELECT * FROM service.users WHERE login=%s", (str(login), ))
+        records = list(cursor.fetchall())
+        if records != []:
+            return render_template('error_page.html', error='User with such login is already registrated!')
+        else:
+            cursor.execute('INSERT INTO service.users (full_name, login, password) VALUES (%s, %s, %s);', (str(name), str(login), str(password)))
+            conn.commit()
+            return redirect('/login/')
+    return render_template('registration.html')
 
